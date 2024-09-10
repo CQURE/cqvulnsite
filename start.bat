7dd63fd1d85163aedd97583a4a281734103b4dea
mkocq <96820301+mkocq@users.noreply.github.com>
2024-09-10 11:16:59 +0200

    Update start.bat
    add lines to stop and remove cotainers contain name "cqvulnsite-main-*"

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
+echo [+] Stoping and removing old lab containers "cqvulnsite-main-*"
+for /F "tokens=*" %%i in ('docker ps -q --filter "name=cqvulnsite-main-"') do docker stop %%i
+for /F "tokens=*" %%i in ('docker ps -a -q --filter "name=cqvulnsite-main-"') do docker rm %%i
 echo [+] Starting lab for lesson %1...
 set DAY=%1
 docker-compose --profile "day%1" up
