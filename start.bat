d2e0c9b35dd54aa2682dc8acd4e1f475e981b2ff
mkocq <96820301+mkocq@users.noreply.github.com>
2025-02-04 09:55:17 +0100

    Update start.bat
    Add checking Docker is running.

--- start.bat
+++ start.bat
@@ -1,4 +1,11 @@
 @echo off
+echo [*] Checking if Docker is running...
+docker info >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [!] Docker is not running. Please start Docker Desktop and try again.
+    exit /b 1
+)
+
 echo [+] Stoping and removing old lab containers "cqvulnsite-main-*"
 for /F "tokens=*" %%i in ('docker ps -q --filter "name=cqvulnsite-main-"') do docker stop %%i
 for /F "tokens=*" %%i in ('docker ps -a -q --filter "name=cqvulnsite-main-"') do docker rm %%i
